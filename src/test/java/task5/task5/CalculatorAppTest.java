458f4ca88ae092ee2466b3b4ab834e63dcee3ab1
Swetha S <swethassivasankaran@gmail.com>
2024-04-03 10:54:07 +0530

    5

--- src/test/java/task5/task5/CalculatorAppTest.java
+++ src/test/java/task5/task5/CalculatorAppTest.java
@@ -6,31 +6,28 @@ import junit.framework.Assert;
 
 public class CalculatorAppTest {
 	@Test
-	@Parameters({"a","b"})
-	public void testAdd(String sa, String sb) {
+	public void testAdd() {
 		CalculatorApp app = new CalculatorApp();
-		int a = Integer.parseInt(sa);
-		int b = Integer.parseInt(sb);
+		int a = 5;
+		int b = 6;
 		int actual=app.addMethod(a, b);
 		int expected=a+b;
 		Assert.assertEquals(actual, expected);
 	}
 	@Test
-	@Parameters({"a","b"})
-	public void testSub(String sa, String sb) {
+	public void testSub() {
 		CalculatorApp app = new CalculatorApp();
-		int a = Integer.parseInt(sa);
-		int b = Integer.parseInt(sb);
+		int a = 6;
+		int b = 5;
 		int actual=app.subMethod(a,b);
 		int expected=a-b;
 		Assert.assertEquals(actual, expected);
 	}
 	@Test
-	@Parameters({"a","b"})
-	public void testMul(String sa, String sb) {
+	public void testMul() {
 		CalculatorApp app = new CalculatorApp();
-		int a = Integer.parseInt(sa);
-		int b = Integer.parseInt(sb);
+		int a = 5;
+		int b = 6;
 		int actual=app.mulMethod(a, b);
 		int expected=a*b;
 		Assert.assertEquals(actual, expected);
